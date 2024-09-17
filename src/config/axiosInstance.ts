a4b1e941657f6b0aca1b454c2697d49584695e98
nuelobeto <nuelobeto@gmail.com>
2024-09-17 05:48:33 +0100

    base url fix

--- src/config/axiosInstance.ts
+++ src/config/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: 'http://staging-api.getmelon.co/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
