f7c2cbf0b28863ffef76a2150142477b6d61db86
nuelobeto <nuelobeto@gmail.com>
2024-10-07 12:27:53 +0100

    Added mobile nav

--- src/components/layouts/dashboard-layout.tsx
+++ src/components/layouts/dashboard-layout.tsx
@@ -5,6 +5,17 @@ import {cn} from '@/lib/utils';
 import {ROUTES} from '@/router/routes';
 import {ScrollArea} from '../ui/scroll-area';
 import {RiNotification3Line} from 'react-icons/ri';
+import {HiOutlineMenuAlt2} from 'react-icons/hi';
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import {X} from 'lucide-react';
 
 export const DashboardLayout = ({
   children,
@@ -18,7 +29,16 @@ export const DashboardLayout = ({
       <SideBar />
       <div className="w-full min-[900px]w-[calc(100vw-256px)] h-full">
         <header className="w-full h-16 border-b border-mountainAsh-6 flex items-center justify-between px-5">
-          <h1 className="font-medium text-xl text-pashBlack-1">{pageTitle}</h1>
+          <div className="flex items-center gap-4">
+            <MobileSidebar>
+              <button>
+                <HiOutlineMenuAlt2 className="text-2xl" />
+              </button>
+            </MobileSidebar>
+            <h1 className="font-medium text-xl text-pashBlack-1">
+              {pageTitle}
+            </h1>
+          </div>
           <div className="flex items-center gap-4">
             <div className="w-11 h-11 rounded-full bg-mountainAsh-8 flex items-center justify-center">
               <RiNotification3Line className="text-2xl" />
@@ -39,24 +59,24 @@ export const DashboardLayout = ({
   );
 };
 
-export const SideBar = () => {
-  const navlinks = [
-    {
-      label: 'Home',
-      url: ROUTES.home,
-      icon: (className?: string) => (
-        <HomeIcon className={className ? className : ''} />
-      ),
-    },
-    {
-      label: 'Settings',
-      url: ROUTES.settings,
-      icon: (className?: string) => (
-        <SettingsIcon className={className ? className : ''} />
-      ),
-    },
-  ];
+const navlinks = [
+  {
+    label: 'Home',
+    url: ROUTES.home,
+    icon: (className?: string) => (
+      <HomeIcon className={className ? className : ''} />
+    ),
+  },
+  {
+    label: 'Settings',
+    url: ROUTES.settings,
+    icon: (className?: string) => (
+      <SettingsIcon className={className ? className : ''} />
+    ),
+  },
+];
 
+export const SideBar = () => {
   return (
     <aside className="min-w-64 h-full bg-pashBlack-1 hidden min-[900px]:block">
       <header className="w-full h-16 flex items-center justify-between px-5">
@@ -101,5 +121,66 @@ export const SideBar = () => {
         ))}
       </div>
     </aside>
+  );
+};
+
+const MobileSidebar = ({children}: {children: React.ReactNode}) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>{children}</SheetTrigger>
+      <SheetContent
+        side={'left'}
+        className="w-full max-w-[350px] bg-pashBlack-1 border-none p-0"
+      >
+        <SheetHeader className="w-full h-16 flex flex-row items-center justify-between px-5 space-y-0 ">
+          <div className="flex items-center gap-2">
+            <Avatar className="w-8 h-8">
+              <AvatarImage src="https://img.utdstc.com/icon/7a5/139/7a5139596d44edaaa15e1a74ed1202442466026aecf262085e3b4cee0061dee6:200" />
+              <AvatarFallback className="text-pashBlack-1">J</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+              <SheetTitle className="font-medium text-base text-white">
+                Sundry foods
+              </SheetTitle>
+              <SheetDescription className="text-xs text-mountainAsh-1 text-left">
+                Jane Doe
+              </SheetDescription>
+            </div>
+          </div>
+          <SheetClose>
+            <X className="text-white" />
+          </SheetClose>
+        </SheetHeader>
+
+        <div className="flex flex-col gap-1 px-5 mt-11">
+          {navlinks.map((link, index) => (
+            <NavLink
+              key={index}
+              to={link.url}
+              className={({isActive}) =>
+                cn(
+                  'px-4 h-12 rounded-xl flex items-center gap-2.5 hover:bg-pashBlack-2/50',
+                  isActive && 'bg-pashBlack-2',
+                )
+              }
+            >
+              {({isActive}) => (
+                <>
+                  {link.icon(isActive ? 'fill-white' : 'fill-pashBlack-8')}
+                  <span
+                    className={cn(
+                      'text-sm',
+                      isActive ? 'text-white' : 'text-pashBlack-8',
+                    )}
+                  >
+                    {link.label}
+                  </span>
+                </>
+              )}
+            </NavLink>
+          ))}
+        </div>
+      </SheetContent>
+    </Sheet>
   );
 };
